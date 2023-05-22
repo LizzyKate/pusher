c725d534d2bba888e9f4d433c47cda324ee5b55a
LizzyKate <onyenekwelizabeth@gmail.com>
2023-05-22 20:31:02 +0100

    testing pusher

--- pages/api/pusher/index.js
+++ pages/api/pusher/index.js
@@ -11,15 +11,22 @@ const pusher = new Pusher({
 export default async function handler(req, res) {
   const { message, username } = req.body;
 
+  console.log("Received request:", req.body); // Log the received request
+
   try {
+    console.log("Triggering Pusher event..."); // Log before triggering Pusher event
+
     await pusher.trigger("presence-channel", "chat-update", {
       message,
       username,
     });
 
+    console.log("Pusher event triggered successfully."); // Log after triggering Pusher event
+
     res.json({ status: 200, message, username }); // Include message and username in the response
   } catch (error) {
-    console.error(error);
+    console.error("Pusher event triggering failed:", error); // Log the error when Pusher event triggering fails
+
     res.status(500).json({ error: "Internal Server Error" }); // Return an error response if there's an exception
   }
 }
